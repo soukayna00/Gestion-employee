a4900e185d4725ad9ec70ca14a80c0059abd0392
soukayna00 <soukayna.zaidi06@gmail.com>
2022-03-03 16:46:23 +0100

    correction des erreur

--- prototype 4/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-   $conn = mysqli_connect('localhost', 'maskoul', 'test123', 'employees_db');
-
-   // check connection
- if(!$conn){
-      echo 'Connection error: ' . mysqli_connect_error(); 
-  }
-?>
