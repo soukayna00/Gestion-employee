fee2250c1c6bde9e171d790e3f0afc497c5c2a22
soukayna00 <soukayna.zaidi06@gmail.com>
2022-03-03 11:17:27 +0100

    reglage

--- prototype 4/employee.php
+++ prototype 4/employee.php
@@ -8,13 +8,16 @@ class Employees {
    private $Gender;
 
 
+   public function getid(){
+    return $this->id;
+  }
 
    public function setid($value){
      $this->id=$value;
   }
 
-  public function getid(){
-    return $this->id;
+  public function getfirst_name(){
+    return $this->first_name;
   }
 
 
@@ -22,31 +25,31 @@ class Employees {
     $this->first_name=$value;
  }
 
- public function getfirst_name(){
-   return $this->first_name;
+ public function getlast_name(){
+  return $this->last_name;
  }
 
  public function setlast_name($value){
   $this->last_name=$value;
 }
 
-public function getlast_name(){
- return $this->last_name;
-}
+
+public function getAge(){
+  return $this->first_name;
+ }
 public function setAge($value){
   $this->first_name=$value;
 }
+  
 
-public function getAge(){
- return $this->first_name;
-}
+public function getgender(){
+  return $this->gender;
+ }
 public function setgender($value){
   $this->first_name=$value;
 }
 
-public function getgender(){
- return $this->gender;
-}
+
 
 }
 
---
+++ prototype 4/employee_manager.php
@@ -0,0 +1,11 @@
+<?php
+include 'employee.php';
+
+
+
+
+
+
+
+
+?>
