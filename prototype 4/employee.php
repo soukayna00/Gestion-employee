b29e2cef395a96c6ef44d08ec6f5955c025f7eba
soukayna00 <soukayna.zaidi06@gmail.com>
2022-03-03 11:03:09 +0100

    class employee

---
+++ prototype 4/employee.php
@@ -0,0 +1,58 @@
+<?php
+
+class Employees {
+  private  $id;
+   private $first_name;
+   private $last_name;
+   private $Age;
+   private $Gender;
+
+
+
+   public function setid($value){
+     $this->id=$value;
+  }
+
+  public function getid(){
+    return $this->id;
+  }
+
+
+  public function setfirst_name($value){
+    $this->first_name=$value;
+ }
+
+ public function getfirst_name(){
+   return $this->first_name;
+ }
+
+ public function setlast_name($value){
+  $this->last_name=$value;
+}
+
+public function getlast_name(){
+ return $this->last_name;
+}
+public function setAge($value){
+  $this->first_name=$value;
+}
+
+public function getAge(){
+ return $this->first_name;
+}
+public function setgender($value){
+  $this->first_name=$value;
+}
+
+public function getgender(){
+ return $this->gender;
+}
+
+}
+
+
+
+
+
+
+?>
