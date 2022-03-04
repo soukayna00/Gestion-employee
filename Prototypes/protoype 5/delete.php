7f19e0eafe4ea783cd30b6f06eab1a34b34a4797
soukayna00 <soukayna.zaidi06@gmail.com>
2022-03-04 12:38:24 +0100

    delete method

---
+++ Prototypes/protoype 5/delete.php
@@ -0,0 +1,11 @@
+<?php
+include 'employeesManager.php';
+
+if(isset($_GET['id'])){
+$employeeManager=new EmployeesManager();
+$id=$_GET['id'];
+$employeeManager->deleteEmployee($id);
+}
+header ('location : index.php');
+
+?>
