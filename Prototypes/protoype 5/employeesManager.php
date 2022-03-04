2a8cba6b84380d3935850c84c3d9bef51c02b2ea
soukayna00 <soukayna.zaidi06@gmail.com>
2022-03-04 12:15:24 +0100

    finished the employee maanager

--- Prototypes/protoype 5/employeesManager.php
+++ Prototypes/protoype 5/employeesManager.php
@@ -4,7 +4,7 @@ class EmployeesManager{
   private $connect =Null ;
   private function getConnect(){
     if(is_null($this->connect)){
-      $this->connect=mysqli_connect('localhost','Soukayna','test123','Prototype 3')
+      $this->connect=mysqli_connect('localhost','Soukayna','test123','Prototype 3');
     }
     else{
        if(!$this->connect){
@@ -22,7 +22,7 @@ class EmployeesManager{
 
     $insertdb="INSERT INTO employees(first_name,last_name,age)VALUES('$firstName','$lastName','$age')";
 
-    mysqli_query($this->getConnect(),$sqlGetData);
+    mysqli_query($this->getConnect(),$insertdb);
   }
   
   public function getAllEmployees(){
@@ -39,14 +39,14 @@ class EmployeesManager{
          $employe->setAge($data['age']);
     array_push($dataArray,$employe);
    }
-    return $dataArray;
+        return $dataArray;
   }
 
 // delete employee
 public function  deleteEmployee($id){
 
   $deleteDB="DELETE FROM employees WHERE ID=$id";
-  mysqli_query($this->getConnect(),$deleteDB)
+  mysqli_query($this->getConnect(),$deleteDB);
 }
 
 // get employee by is
@@ -57,16 +57,27 @@ public function getById($id){
 
   $employee =new Employees();
   $employee->setId($employeeData['ID']);
-  
+  $employee->setfirstName($employeeData['first_name']);
+  $employee->setlastName($employeeData['last_name']);
+  $employee->setAge($employeeData['age']);
 
 
-
-
+  return $employee;
 
 }
 
 
+public function modifyEmployee($id,$first_name,$last_name,$age){
+  $updateDB="UPDATE employees SET first_name='$first_name',
+            last_name='$last_name',age='$age
+            WHERE ID=$id";
+    mysqli_query($this->getConnect(),$updateDB);
+    if(mysqli_error($this->getConnect())){
+     $message="data base connection  error".mysqli_error($this->getConnect());
+     throw new Exception($message);
+    }
 
+}
 
 
 
