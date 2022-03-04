6fd695143bc511e02e9f5015258a1413efae23b8
soukayna00 <soukayna.zaidi06@gmail.com>
2022-03-04 12:30:03 +0100

    insert methode

---
+++ Prototypes/protoype 5/insert.php
@@ -0,0 +1,34 @@
+<?php
+include  'employeesManager.php';
+
+
+if(!empty($_POST)){
+  $employee= new Employees();
+  $employeeManager=new EmployeesManager();
+  $employee->setfirstName($_POST['first_name']);
+  $employee->setlastName($POST['last_name']);
+  $employee->setAge($POST['age']);
+  $employeeManager->insertEmployees($employee);
+  header("location:index.php");
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <h1>Add Employee</h1>
+  <form method="post">
+  <input type="text" name='first_name'required>
+  <input type="text"  name="last_name" required>
+  <input type='text' name="age" required>
+  <input type="submit" value= "add">
+
+  </form>
+
+</body>
+</html>
